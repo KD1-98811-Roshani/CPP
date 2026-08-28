311d7d3fe898a2311aad76651383a0147fbb8dd1
Roshani Dhembare <roshanidhembare233@gmail.com>
2026-08-28 15:03:40 +0530

    Added Assignment 3

--- Assignment2_2.cpp
+++ Assignment2_2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class Volume
+class Tollbooth
 {
     private:
     int totalCars;
@@ -9,24 +9,48 @@ class Volume
 
     public:
 
-    Volume(void)
+    Tollbooth( void ) : totalCars( 0 ), totalCash( 0 )
     {
-        this->totalCars = 0;
-        this->totalCash = 0;
     }
 
-    void payingCar(void)
+    void payingCar( void )
     {
         this->totalCars++;
         this->totalCash = this->totalCash + 0.50;
     }
 
-    void nopayCar(void)
+    void nopayCar( void )
     {
         this->totalCars++;
     }
 
-    void printOnConsole(void)
+    void acceptRecord( void )
+    {
+        int choice;
+
+        cout<<"1. Paying Car"<<endl;
+        cout<<"2. Non Paying Car"<<endl;
+
+        cout<<"Enter choice : ";
+        cin>>choice;
+
+        switch(choice)
+        {
+            case 1:
+                this->payingCar();
+                break;
+
+            case 2:
+                this->nopayCar();
+                break;
+
+            default:
+                cout<<"Invalid choice"<<endl;
+                break;
+        }
+    }
+
+    void printRecord( void )
     {
         int payingCars;
         int nonPayingCars;
@@ -34,60 +58,19 @@ class Volume
         payingCars = (int)(this->totalCash / 0.50);
         nonPayingCars = this->totalCars - payingCars;
 
-        cout<<"Total Cars = "<<this->totalCars<<endl;
-        cout<<"Total Cash = "<<this->totalCash<<endl;
-        cout<<"Paying Cars = "<<payingCars<<endl;
-        cout<<"Non Paying Cars = "<<nonPayingCars<<endl;
+        cout<<"Total Cars : "<<this->totalCars<<endl;
+        cout<<"Total Cash : "<<this->totalCash<<endl;
+        cout<<"Paying Cars : "<<payingCars<<endl;
+        cout<<"Non Paying Cars : "<<nonPayingCars<<endl;
     }
 };
 
-int menulist(void)
-{
-    int choice;
-
-    cout<<"0. Exit\n";
-    cout<<"1. Paying Car\n";
-    cout<<"2. Non Paying Car\n";
-    cout<<"3. Display Details\n";
-
-    cin>>choice;
-    return choice;
-}
-
 int main()
 {
-    int choice;
-    Volume v1;
+    Tollbooth t1;
 
-    while((choice=menulist())!=0)
-    {
-        switch(choice)
-        {
-            case 1:
-            {
-                v1.payingCar();
-                break;
-            }
-
-            case 2:
-            {
-                v1.nopayCar();
-                break;
-            }
-
-            case 3:
-            {
-                v1.printOnConsole();
-                break;
-            }
-
-            default:
-            {
-                cout<<"Enter only 0,1,2,3"<<endl;
-                break;
-            }
-        }
-    }
+    t1.acceptRecord();
+    t1.printRecord();
 
     return 0;
 }
